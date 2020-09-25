e028d54bf746c194ee580db78145b46b06e21402
Chris Cole <ccole@lambdaoil.local>
2020-09-25 14:06:58 -0500

    removing unused pkgs

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
 
 interface footerProps {
