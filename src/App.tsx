6a976bf66c16586a699b6dcb7a9f15717ebc4c8b
Chris Cole <ccole@lambdaoil.local>
2020-09-25 14:12:45 -0500

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Home } from "./components/home";
+import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 
 export const App = () => {
   return (
     <>
+      <Header />
       <main>
         <Home />
       </main>
