a6e2978c453811cd9261789743f239b767b6fe28
Chris Cole <ccole@lambdaoil.local>
2020-09-28 08:37:54 -0500

    adding sentence

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -43,8 +43,9 @@ export const Hero = () => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <Typography variant="subtitle1" color="textSecondary">
-            I'm a software engineer based in Houston, TX (yeehaw) who loves
-            building websites, applications, and everything in between.
+            I'm based in Houston, TX (yeehaw) who loves building websites,
+            applications, and everything in between. I really appreciate you
+            stopping by my website today!
           </Typography>
         </Grid>
         <Grid item xs={12}>
