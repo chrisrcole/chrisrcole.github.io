0a49ebeee4022504a27ff6a1b34f4d4051df47c5
Chris Cole <ccole@lambdaoil.local>
2020-09-28 13:58:17 -0500

    Updates

---
+++ precache-manifest.8cd6e176e27548041c2043690674b6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bc8cbb383a39f8c6115fd404c990ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9380d57b4b1f19df2cf",
+    "url": "/static/js/2.157c1152.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.157c1152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd43ea3aaa3cabfd2fdd",
+    "url": "/static/js/main.cc896f98.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "219701ada1053294e4556d5ee78ded68",
+    "url": "/static/media/logo-short.219701ad.svg"
+  },
+  {
+    "revision": "87b537e75bffeb9b6090baf1a54229fe",
+    "url": "/static/media/portrait.87b537e7.jpg"
+  }
+]);
